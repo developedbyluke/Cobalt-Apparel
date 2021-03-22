c9116239a7d3109f765e38d1c3a4fde9d3cd83c7
developedbyluke <developedbyluke@gmail.com>
2021-03-22 20:53:52 +0000

    5. Nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -6,18 +6,73 @@ const Nav = () => (
     <Logo id="logo">
       <a href="#">Cobalt Apparel</a>
     </Logo>
+    <nav>
+      <ul>
+        <li className="start">
+          <a href="#">Brands</a>
+        </li>
+        <li>
+          <a href="#">Shoes</a>
+        </li>
+        <li>
+          <a href="#">Clothing</a>
+        </li>
+        <li>
+          <a href="#">Accessories</a>
+        </li>
+        <li className="end">
+          <a href="#">Sale</a>
+        </li>
+      </ul>
+    </nav>
+    <div className="nav-buttons">
+      <h4>Currency</h4>
+      <h4>Search</h4>
+      <h4>Cart</h4>
+    </div>
   </Header>
 );
 
 const Header = styled.header`
+  color: white;
   z-index: 2;
   position: absolute;
   width: 100%;
   background-color: rgba(0, 0, 0, 0.783);
-  padding: 0.8rem 3rem;
+  padding: 0.8rem 4.5rem;
   height: 8vh;
   display: flex;
   align-items: center;
+  flex-direction: row;
+  justify-content: space-between;
+  ul {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    cursor: pointer;
+
+    a {
+      font-size: 1rem;
+      padding: 0 2rem;
+    }
+    li::after {
+      content: '|';
+      color: grey;
+    }
+    .end {
+      ::after {
+        content: '';
+      }
+      a {
+        padding-right: 0 0 0 2rem;
+      }
+    }
+    .start {
+      a {
+        padding: 0 2rem 0 0;
+      }
+    }
+  }
 `;
 
 const Logo = styled.h1`
@@ -27,7 +82,7 @@ const Logo = styled.h1`
     color: white;
     font-family: 'Caveat', cursive;
     font-weight: 400;
-    font-size: 1.6rem;
+    font-size: 2rem;
   }
 `;
 
